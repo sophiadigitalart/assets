0d89b7446769396e33a183a119de8aa6d435f271
Bruce LANE <bruce@batchass.fr>
2018-11-22 05:35:33 +0100

    isf

--- glsl/isf/fftMatrixProduct.fs
+++ glsl/isf/fftMatrixProduct.fs
@@ -1,5 +1,5 @@
 /*{
-	"CREDIT" : "fftMatrixProduct by Unknown",
+	"CREDIT" : "fftMatrixProduct by ",
 	"CATEGORIES" : [
 		"ci"
 	],
@@ -8,13 +8,6 @@
 		{
 			"NAME": "inputImage",
 			"TYPE" : "image"
-		},
-		{
-			"NAME": "iZoom",
-			"TYPE" : "float",
-			"MIN" : 0.0,
-			"MAX" : 1.0,
-			"DEFAULT" : 1.0
 		},
 		{
 			"NAME": "iSteps",
@@ -29,7 +22,8 @@
 			"DEFAULT" : [0.0, 0.0],
 			"MAX" : [640.0, 480.0],
 			"MIN" : [0.0, 0.0]
-		},
+		}
+,
 		{
 			"NAME": "iColor", 
 			"TYPE" : "color", 
@@ -43,4 +37,4 @@
 	],
 }
 */
-void mainImage(void){vec2 uv = fragCoord.xy / RENDERSIZE.xy;uv = abs(2.0*(uv - 0.5));vec4 t1 = IMG_THIS_PIXEL(inputImage, vec2(uv[0], 0.1));vec4 t2 = IMG_THIS_PIXEL(inputImage, vec2(uv[1], 0.1));float fft = t1[0] * t2[0];fragColor = vec4(sin(fft*3.141*2.5), sin(fft*3.141*2.0), sin(fft*3.141*1.0), 1.0);}void main(void) { mainImage(gl_FragColor, gl_FragCoord.xy); }
+void main(void){vec2 uv = gl_FragCoord.xy / RENDERSIZE.xy;uv = abs(2.0*(uv - 0.5));vec4 t1 = IMG_THIS_PIXEL(inputImage, vec2(uv[0], 0.1));vec4 t2 = IMG_THIS_PIXEL(inputImage, vec2(uv[1], 0.1));float fft = t1[0] * t2[0];gl_FragColor = vec4(sin(fft*3.141*2.5), sin(fft*3.141*2.0), sin(fft*3.141*1.0), 1.0);}
